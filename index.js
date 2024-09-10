febbb3196ab71b280ea156fdb306c930005bc050
Maria Lukowich <mlukowich27@gmail.com>
2024-09-10 13:24:37 -0600

    adding another route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App</h1>`);
 });
 
+app.get("/videos", (req, res) => {
+    res.send("This is a video route")
+})
+
 app.listen(PORT, () => {
    console.log(`app running on port ${PORT}`)
 });
